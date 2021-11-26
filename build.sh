aafbbc9b0356e879962424dbf7abb456d2d95913
myminseok <myminseok@users.noreply.github.com>
2021-11-26 14:55:28 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t jupyter-scipy-custom-docker:mvp1 --file Dockerfile.mvp1 .
+docker tag jupyter-scipy-custom-docker:mvp1 platform-harbor.mvp.bsl.local/kubeflow/jupyter-scipy-custom-docker:mvp1
+docker push jupyter-scipy-custom-docker:mvp1 platform-harbor.mvp.bsl.local/kubeflow/jupyter-scipy-custom-docker:mvp1
